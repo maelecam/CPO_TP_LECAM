08c4f0749e1fb131f562088deee535b73098a1c0
33604 <33604@10.3.60.16>
2024-09-20 08:56:34 +0200

    Création d'un nouveau projet versionné

---
+++ premierProjetSurGithib/src/premierprojetsurgithib/PremierProjetSurGithib.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package premierprojetsurgithib;
+
+/**
+ *
+ * @author 33604
+ */
+public class PremierProjetSurGithib {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
