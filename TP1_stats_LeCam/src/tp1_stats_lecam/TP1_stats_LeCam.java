16608cbe32975eed69540a710b8c066d6ff11e32
33604 <33604@10.3.60.16>
2024-09-23 18:33:57 +0200

    Création d'un nouveau projet versionné

---
+++ TP1_stats_LeCam/src/tp1_stats_lecam/TP1_stats_LeCam.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_stats_lecam;
+
+/**
+ *23/09/2024
+ * @author Mael Le Cam
+ * TDA
+ */
+public class TP1_stats_LeCam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int tab[] = new int [6]; 
+     
+    }
+    
+}
