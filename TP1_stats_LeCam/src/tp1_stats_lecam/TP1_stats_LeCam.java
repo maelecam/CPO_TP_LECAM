f34d11bb72ef800afc105de4dc7d003b947e0fd3
33604 <33604@10.3.60.16>
2024-09-24 18:52:02 +0200

    Création d'un nouveau projet versionné

--- TP1_stats_LeCam/src/tp1_stats_lecam/TP1_stats_LeCam.java
+++ TP1_stats_LeCam/src/tp1_stats_lecam/TP1_stats_LeCam.java
@@ -4,10 +4,12 @@
  */
 package tp1_stats_lecam;
 
+import java.util.Scanner;
+
 /**
- *23/09/2024
- * @author Mael Le Cam
- * TDA
+ * 23/09/2024
+ *
+ * @author Mael Le Cam TDA
  */
 public class TP1_stats_LeCam {
 
@@ -15,8 +17,27 @@ public class TP1_stats_LeCam {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     int tab[] = new int [6]; 
-     
+        int[] tab = new int[6];
+        System.out.println("Initial dice frequencies:");
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println("Face " + (i + 1) + ": " + tab[i]);
+        }
+
+        // demander a l'utlisateur de saisir la frequence
+        int m;
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Saisissez le nombre de lancers (simulations) :");
+        m = sc1.nextInt();
+
+        // lancer de dé
+        for (int i = 0; i < m; i++) {
+            int result = (int) (Math.random() * 6);
+            tab[result]++;
+        }
+
+        System.out.println("Resultat nombre de face obtenue apres simulation :");
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println("Face " + (i + 1) + ": " + tab[i]);
+        }
     }
-    
 }
