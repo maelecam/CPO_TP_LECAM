26d761e83a71c091119042d8dc9364d92498a885
33604 <33604@10.3.60.16>
2024-09-26 10:22:24 +0200

    Création d'un nouveau projet versionné

--- TP1_guessMyNumber_LeCam/src/tp1_guessmynumber_lecam/TP1_guessMyNumber_LeCam.java
+++ TP1_guessMyNumber_LeCam/src/tp1_guessmynumber_lecam/TP1_guessMyNumber_LeCam.java
@@ -18,7 +18,7 @@ public class TP1_guessMyNumber_LeCam {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int saisie;
+        int valeur1;
         System.out.println("Saisissez le niveau de difficulté :");
         System.out.println("1) Facile");
         System.out.println("2) Normal");
@@ -26,12 +26,12 @@ public class TP1_guessMyNumber_LeCam {
         Scanner sc1 = new Scanner(System.in);
         System.out.println("Veillez saisir un nombre :");
         saisie = sc1.nextInt();
-        switch (saisie){
-            case 1 : saisie = Facile(n); 
-            case 2 : saisie = CelciusVersFarenheit(valeur1);
-            case 3 : saisie = KelvinVersCelcius(valeur1);   
+        switch (valeur1){
+            case 1 : valeur1= Facile(valeur1); 
+            case 2 : valeur1 = Normal(valeur1);
+            case 3 : valeur1 = Difficile(valeur1);   
     }
-    public static void facile(String[] args) {
+    public static void Facile(String[] args) {
       Random generateurAleat = new Random();  
       int n = generateurAleat.nextInt(100);
       int valeur;
@@ -57,5 +57,59 @@ public class TP1_guessMyNumber_LeCam {
             }
       } 
     }
-    
+    public static void Difficile(String[] args) {
+      Random generateurAleat = new Random();  
+      int n = generateurAleat.nextInt(100);
+      int valeur;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Veillez saisir un nombre compris entre 0 et 100 :");
+      valeur = sc.nextInt();
+      int compteur;
+      compteur = 20;
+      while ( n != valeur){
+        compteur = compteur - 1;
+        System.out.println("Nombre tentatives :" +compteur);
+        if (compteur == 0){
+            break;
+        }
+        if (valeur > n){
+            System.out.println("trop grand");
+            System.out.println("Veillez saisir un nombre compris entre 0 et 100 :");
+            valeur = sc.nextInt();
+            compteur = compteur + 1;
+          } if ( valeur < n){
+            System.out.println("Trop petit");
+            System.out.println("Veillez saisir un nombre compris entre 0 et 100 :");
+            valeur = sc.nextInt();
+            } if (valeur == n){
+                System.out.println("gagne");
+            }
+      } 
+    }
+        public static void Normal(String[] args) {
+      Random generateurAleat = new Random();  
+      int n = generateurAleat.nextInt(100);
+      int valeur;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Veillez saisir un nombre compris entre 0 et 100 :");
+      valeur = sc.nextInt();
+      int compteur;
+      compteur = 0;
+      while ( n != valeur){
+        compteur = compteur + 1;
+        System.out.println("Nombre tentatives :"+compteur);
+        if (valeur > n){
+            System.out.println("trop grand");
+            System.out.println("Veillez saisir un nombre compris entre 0 et 100 :");
+            valeur = sc.nextInt();
+            compteur = compteur + 1;
+          } if ( valeur < n){
+            System.out.println("Trop petit");
+            System.out.println("Veillez saisir un nombre compris entre 0 et 100 :");
+            valeur = sc.nextInt();
+            } if (valeur == n){
+                System.out.println("gagne");
+            }
+      } 
+    }
 }
