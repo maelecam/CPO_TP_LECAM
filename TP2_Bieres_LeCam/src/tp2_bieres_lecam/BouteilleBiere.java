c5afbe6c3800a2c141f17b02e772fb100d3b06ab
33604 <33604@10.3.60.16>
2024-10-07 10:28:04 +0200

    Création d'un nouveau projet versionné

--- TP2_Bieres_LeCam/src/tp2_bieres_lecam/BouteilleBiere.java
+++ TP2_Bieres_LeCam/src/tp2_bieres_lecam/BouteilleBiere.java
@@ -4,18 +4,51 @@
  */
 package tp2_bieres_lecam;
 
+import java.io.Console;
+
 /**
  *
  * @author 33604
  */
 public class BouteilleBiere {
+
     String Nom;
-    float degreAlcool;
+    double degreAlcool;
     String brasserie;
     boolean ouverte;
-    public void lireEtiquette(){
-        System.out.println("Bouteille de "+ Nom +"("+degreAlcool+"degres)\nBrasserie : "+brasserie);
-        
-}
+
+    public void lireEtiquette() {
+        System.out.println("Bouteille de " + Nom + "(" + degreAlcool + "degres)\nBrasserie : " + brasserie);
+    }
+
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        String nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+    }
+
+    public boolean Décapsuler() {
+        if (!ouverte) {
+            ouverte = false;
+            return true;
+        } else {
+            Console.WriteLine("Erreur : bière déjà ouverte");// erreur etape 13
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degrés) Ouverte ? ";
+        if (ouverte == true) {
+            chaine_a_retourner += "oui";
+        } else {
+            chaine_a_retourner += "non";
+        }
+        return chaine_a_retourner;
+    }
 }
 
+// finir etape 14 et 13 de cette exo
