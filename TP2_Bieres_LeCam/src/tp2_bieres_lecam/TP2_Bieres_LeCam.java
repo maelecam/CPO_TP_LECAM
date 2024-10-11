7770d5a83dad1d453caf426168e2a53661149786
33604 <33604@10.3.60.16>
2024-10-11 10:18:20 +0200

    Création d'un nouveau projet versionné

--- TP2_Bieres_LeCam/src/tp2_bieres_lecam/TP2_Bieres_LeCam.java
+++ TP2_Bieres_LeCam/src/tp2_bieres_lecam/TP2_Bieres_LeCam.java
@@ -5,7 +5,7 @@
 package tp2_bieres_lecam;
 
 /**
- *
+ *11/10/2024
  * @author 33604
  */
 public class TP2_Bieres_LeCam {
@@ -14,10 +14,15 @@ public class TP2_Bieres_LeCam {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",7.0 ,"Dubuisson") ;
-        BouteilleBiere deuxiemeBiere = new BouteilleBiere("Leffe",6.6 ,"Abbaye de Leffe") ;
-        BouteilleBiere troisiemeBiere = new BouteilleBiere("La 16", 8.0 , "Montpel");
-        BouteilleBiere quatriemeBiere = new BouteilleBiere("Hinano", 9.0, "Something");
-        BouteilleBiere LAPLUSHORRIBLE = new BouteilleBiere("la heineken", 9.9, "J'sais pas");
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",7.0 ,"Dubuisson",false) ;
+        System.out.println(uneBiere);
+        BouteilleBiere deuxiemeBiere = new BouteilleBiere("Leffe",6.6 ,"Abbaye de Leffe", true) ;
+        System.out.println(deuxiemeBiere);
+        BouteilleBiere troisiemeBiere = new BouteilleBiere("La 16", 8.0 , "Montpel",false);
+        System.out.println(troisiemeBiere);
+        BouteilleBiere quatriemeBiere = new BouteilleBiere("Hinano", 9.0, "Something", true);
+        System.out.println(quatriemeBiere);
+        BouteilleBiere LAPLUSHORRIBLE = new BouteilleBiere("la heineken", 9.9, "J'sais pas",false);
+        System.out.println(LAPLUSHORRIBLE);
     }
 }
