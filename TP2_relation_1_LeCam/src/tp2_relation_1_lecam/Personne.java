d2128b1d642db296e9bcc52b13c77b9e3065e10d
33604 <33604@10.3.60.16>
2024-10-16 09:33:26 +0200

    Création d'un nouveau projet versionné

---
+++ TP2_relation_1_LeCam/src/tp2_relation_1_lecam/Personne.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_lecam;
+
+/**
+ *
+ * @author 33604
+ */
+public class Personne {
+    String nom;
+    String prenom;
+
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    @Override
+    public String toString() {
+        return "Personne{" + "nom=" + nom + ", prenom=" + prenom + '}';
+    }
+}
