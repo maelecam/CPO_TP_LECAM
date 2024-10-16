02d9420ce31a0128e8d35cc4d55c342c6478443a
33604 <33604@10.3.60.16>
2024-10-16 08:36:44 +0200

    Création d'un nouveau projet versionné

---
+++ NouvelleInterfaceGraphique/src/nouvelleinterfacegraphique/NouvelleInterfaceGraphique.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nouvelleinterfacegraphique;
+
+/**
+ *
+ * @author 33604
+ */
+public class NouvelleInterfaceGraphique {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PremiereFenetre2 f = new PremiereFenetre2();
+      f.show();   
+    }
+    
+}
