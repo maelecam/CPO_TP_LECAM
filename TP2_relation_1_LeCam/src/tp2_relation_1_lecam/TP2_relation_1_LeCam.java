00c24cf18c0609db04440e3edefe35098faaa4b4
33604 <33604@10.3.60.16>
2024-10-16 09:33:43 +0200

    Création d'un nouveau projet versionné

---
+++ TP2_relation_1_LeCam/src/tp2_relation_1_lecam/TP2_relation_1_LeCam.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_relation_1_lecam;
+
+/**
+ *
+ * @author 33604
+ */
+public class TP2_relation_1_LeCam {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
