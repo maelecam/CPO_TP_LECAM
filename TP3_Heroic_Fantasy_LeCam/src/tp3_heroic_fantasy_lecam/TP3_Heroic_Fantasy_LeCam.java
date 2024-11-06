b685bdb8978843cb9133a7c0e0142f398c91a7f4
33604 <33604@10.3.60.16>
2024-11-06 15:30:05 +0100

    TP3 Main

---
+++ TP3_Heroic_Fantasy_LeCam/src/tp3_heroic_fantasy_lecam/TP3_Heroic_Fantasy_LeCam.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_heroic_fantasy_lecam;
+
+/**
+ *
+ * @author 33604
+ */
+public class TP3_Heroic_Fantasy_LeCam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Epee premiere_epee = new Epee("Excalibur",7)
+}
