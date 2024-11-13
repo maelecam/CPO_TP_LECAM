5ec1fda0f563a8bd6d04b88a0e88b8dfa7466f7c
33604 <33604@10.3.60.16>
2024-11-13 13:52:13 +0100

    TP3 classe Epee

---
+++ TP3_Heroic_Fantasy_LeCam/src/Armes/Epee.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Armes;
+
+import Armes.Arme;
+
+/**
+ *
+ * @author 33604
+ */
+public class Epee extends Arme {
+    private int finesse;
+
+    public Epee(String nom, int Attaque, int finesse) {
+        super(nom, Attaque);
+        this.finesse = finesse;
+        if (finesse < 100){
+            this.finesse = finesse ;
+        } else {
+            this.finesse = 99;
+        }
+    }
+    public int finesseEpee(){
+        return finesse;
+    }
+    @Override
+    public String toString() {
+        return super.toString()+ ", Finesse : " + finesse;
+    }
+    
+}
