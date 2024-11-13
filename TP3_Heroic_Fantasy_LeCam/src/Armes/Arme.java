3b38639adf52c3761f43b47ca2d974f411e76f77
33604 <33604@10.3.60.16>
2024-11-13 13:52:37 +0100

    TP3 classe Arme

---
+++ TP3_Heroic_Fantasy_LeCam/src/Armes/Arme.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Armes;
+
+/**
+ *
+ * @author 33604
+ */
+public class Arme {
+    private String nom;
+    private int NiveauAttaque ;
+
+    public Arme(String nom, int Attaque) {
+        this.nom = nom;
+        this.NiveauAttaque = NiveauAttaque;
+        if (NiveauAttaque <= 100){
+            this.NiveauAttaque = NiveauAttaque ;
+        } else {
+            this.NiveauAttaque = 100 ;
+        }
+    }
+    public String AvoirNom(){
+        return this.nom;
+    }
+    @Override
+    public String toString() {
+        return "Arme{" + "nom=" + nom + ", Attaque=" + NiveauAttaque + '}';
+    }
+
+    Arme elementAt(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
