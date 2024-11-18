95424e0b1190b22a81aabae246ab566f6ce0927b
33604 <33604@10.3.60.16>
2024-11-18 19:42:59 +0100

    TP3 classe Magicien

---
+++ TP3_Heroic_Fantasy_LeCam/src/Personnages/Magicien.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personnages;
+
+/**
+ *
+ * @author 33604
+ */
+public class Magicien extends Personnage {
+
+    private boolean confirme;
+    private String palier;
+    
+    public Magicien(String nom, int niveauDeVie, boolean confirme) {
+        super(nom, niveauDeVie);
+        this.confirme = confirme;
+        this.palier = confirme ? "Confirmé" : "Novice";
+    }
+
+    public boolean isConfirme() {
+        return confirme;
+    }
+
+    public String getPalier() {
+        return palier;
+    }
+
+    public void setConfirme(boolean confirme) {
+        this.confirme = confirme;
+        this.palier = confirme ? "Confirmé" : "Novice";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Magicien{" + "confirme=" + confirme + ", palier=" + palier + '}';
+    }
+}
+
